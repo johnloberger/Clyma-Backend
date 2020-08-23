d72703559d5271d9138ee0ad93fe79f94f820b47
johnloberger <johnloberger@gmail.com>
2020-08-23 18:36:46 -0500

    more user create method tweak

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,7 @@ class UsersController < ApplicationController
   
   def create
     user = User.create(user_params)
-      if user.valid?
-        token = encode_token(user_id: user.id)
-        render json: user
-      else 
-        render json: { error: 'failed to create user'}, status: :not_acceptable
-    end
+    render json: user
   end
 
   def index
